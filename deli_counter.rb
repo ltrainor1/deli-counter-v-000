105031feeecde2919cdc3a6a3aaf3063154da0ef
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-24 23:30:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array == []
+    puts "The line is currently empty."
+  else 
+    i = 0 
+    sentence = "The line is currently\n"
+    while i < array.length 
+      sentence += "#{i + 1}. {array[i]} "
+      i += 1 
+    end 
+    puts sentence 
+  end 
+end 
+  
+def take_a_number(array, person)
+  array << person 
+  puts "Welcome, #{person}. You are number #{array.length} in line."
+end 
+
+def now_serving(array)
+  puts "Currently serving #{array[0]}."
+end 
+
+
+  
